80986ec2bcf18baf866c39e9b2722578c5b4eab2
Jason Hinchcliffe <hinchy@gmail.com>
2020-08-28 17:05:27 +1000

    chore: Fix formatting and rename method on Sheet

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
     @topic_count = Topic.all.count
-    @spreadsheet_info = SheetsClient.new.get_rows
+    @spreadsheet_info = SheetsClient.new.rows
   end
 
   def test
--- app/utilities/sheets_client.rb
+++ app/utilities/sheets_client.rb
@@ -1,24 +1,24 @@
 require 'google/apis/sheets_v4'
 require 'googleauth' # https://github.com/googleapis/google-auth-library-ruby
-class SheetsClient
 
+class SheetsClient
   # From the Google client json file
   CREDS = {
-    "type": ENV['TYPE'],
-    "project_id": ENV['PROJECT_ID'],
-    "private_key_id": ENV['PRIVATE_KEY_ID'],
-    "private_key": ENV['PRIVATE_KEY'],
+    "auth_provider_x509_cert_url": ENV['AUTH_PROVIDER_X509_CERT_URL'],
+    "auth_uri": ENV['AUTH_URI'],
     "client_email": ENV['CLIENT_EMAIL'],
     "client_id": ENV['CLIENT_ID'],
-    "auth_uri": ENV['AUTH_URI'],
+    "client_x509_cert_url": ENV['CLIENT_X509_CERT_URL'],
+    "private_key_id": ENV['PRIVATE_KEY_ID'],
+    "private_key": ENV['PRIVATE_KEY'],
+    "project_id": ENV['PROJECT_ID'],
     "token_uri": ENV['TOKEN_URI'],
-    "auth_provider_x509_cert_url": ENV['AUTH_PROVIDER_X509_CERT_URL'],
-    "client_x509_cert_url": ENV['CLIENT_X509_CERT_URL']
+    "type": ENV['TYPE']
   }
 
-  SPREADSHEET_SCOPE = 'https://www.googleapis.com/auth/spreadsheets'
+  SPREADSHEET_SCOPE = 'https://www.googleapis.com/auth/spreadsheets'.freeze
 
-  def get_rows
+  def rows
     content = sheet_service
 
     content.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
@@ -26,16 +26,11 @@ class SheetsClient
       scope: SPREADSHEET_SCOPE)
 
     content.authorization.fetch_access_token!
-    # Service methods: https://googleapis.dev/ruby/google-api-client/latest/Google/Apis/ContentV2/ShoppingContentService.html
 
-    spreadsheet_id = "1eu1Dk67gKnrIgQQ9Fm0Y-RCMzRfZf1UaTQzEt7hjWp0"
-    range = "Sheet1!A1:E"
-    # response = content.get_spreadsheet_values spreadsheet_id, range
+    spreadsheet_id = '1eu1Dk67gKnrIgQQ9Fm0Y-RCMzRfZf1UaTQzEt7hjWp0'
+    range = 'Sheet1!A1:E'
+
     response = content.get_spreadsheet_values(spreadsheet_id, range)
-
-    # binding.pry
-    # puts "Name, Major:"
-    # puts "No data found." if response.values.empty?
 
     response.values
   end
@@ -47,7 +42,7 @@ class SheetsClient
   end
 
   def creds_file
-    tempfile = Tempfile.new("changed")
+    tempfile = Tempfile.new('changed')
     tempfile.write CREDS.to_json
     tempfile.rewind
     tempfile
